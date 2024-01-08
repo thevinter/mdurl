3a1c701c4f1cf1a4fef85c1d9dc137ed4ff1e909
thevinter <contact@thevinter.com>
2024-01-08 20:35:28 +0100

    chore: I'm an idiot 2

--- mdurl-frontend/src/MarkdownPage.js
+++ mdurl-frontend/src/MarkdownPage.js
@@ -17,7 +17,7 @@ const MarkdownPage = () => {
     const fetchMarkdown = async () => {
       try {
         const response = await fetch(
-          `http://65.109.13.255:5000/markdown/${fileId}.md`
+          `http://65.109.13.225:5000/markdown/${fileId}.md`
         );
 
         if (!response.ok) {
